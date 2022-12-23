7639c1e0e1fd072ec845f0a4ad668eb13464cd65
Md Mijanur Rahman <md.rahman@fau.de>
2022-12-23 20:41:23 +0100

    export as csv

--- evaluate.py
+++ evaluate.py
@@ -3,14 +3,22 @@ from utils.parser import get_given_parameters_parser
 from utils.scorer import get_scorer
 
 import numpy as np
-
+import pandas as pd
 
 def main(args):
     print("Evaluate model " + args.model_name)
 
     predictions = get_predictions_from_file(args)
     scorer = get_scorer(args)
-
+    #from here
+    stacked_predictions = np.vstack(predictions)
+    #conver to dataframe
+    df = pd.DataFrame(stacked_predictions)
+    #rename columns
+    df.columns = ['truth', 'pred_0', 'pred_1']
+    #save to csv
+    name = args.model_name + "_prediction_" + args.dataset + ".csv"
+    df.to_csv(name, index=False)
     for pred in predictions:
         # [:,0] is the truth and [:,1:] are the prediction probabilities
 
